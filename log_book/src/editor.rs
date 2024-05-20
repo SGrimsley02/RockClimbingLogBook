96e2b3155c6791fbf4ee87f5d47c9d59a1a9585c
SGrimsley02 <sam.grimsley@gmail.com>
2024-05-20 17:57:01 -0500

    Added movable cursor

--- log_book/src/editor.rs
+++ log_book/src/editor.rs
@@ -1,42 +1,140 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+use core::cmp::min;
+use crossterm::event::{read, Event::{self, Key}, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+mod terminal;
+use terminal::{Terminal, Size, Position};
+use std::io::Error;
 
+
+
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Location {
+    pub x: usize,
+    pub y: usize,
+}
+
+#[derive(Default)]
 pub struct Editor {
     should_quit: bool,
+    location: Location,
 }
 
 impl Editor {
-    pub fn default() -> Self {
-        Editor{should_quit: false}
-    }
-    
     pub fn run(&mut self) {
-        if let Err(err) = self.repl() {
-            panic!("{err:#?}");
-        }
-        print!("Goodbye!\r\n");
+        Terminal::initialize().unwrap();
+        let result = self.repl();
+        Terminal::terminate().unwrap();
+        result.unwrap();
     }
 
-    fn repl(&mut self) -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
+    fn repl(&mut self) -> Result<(), Error> {
         loop {
-            if let Key(KeyEvent {
-                code, modifiers, kind, state
-            }) = read()? {
-                println!("Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?} State: {state:?} \r");
-                match code {
-                    Char('q') if modifiers == KeyModifiers::CONTROL => {
-                        self.should_quit = true;
-                    }
-                    _ => {},
-                }
-            }
+            self.refresh_screen()?;
             if self.should_quit {
                 break;
             }
+            let event = read()?;
+            self.evaluate_event(&event)?;
         }
-        disable_raw_mode()?;
         Ok(())
     }
+
+    fn move_point(&mut self, key: KeyCode) -> Result<(), Error> {
+        let Location{mut x, mut y} = self.location;
+        let Size{width, height} = Terminal::size()?;
+        match key {
+            KeyCode::Up => y = y.saturating_sub(1),
+            KeyCode::Down => y = min(y + 1, height),
+            KeyCode::Left => x = x.saturating_sub(1),
+            KeyCode::Right => x = min(x + 1, width),
+            KeyCode::Home => x = 0,
+            KeyCode::End => x = width.saturating_sub(1),
+            _ => (),
+        }
+        self.location = Location{x, y};
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self, event: &Event) -> Result<(), Error> {
+        if let Key(KeyEvent {
+            code, 
+            modifiers, 
+            kind: KeyEventKind::Press, //Windows compatibility
+            ..
+        }) = event {
+            match code {
+                //Quit
+                KeyCode::Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                },
+                //Move caret
+                KeyCode::Up
+                | KeyCode::Down
+                | KeyCode::Left
+                | KeyCode::Right
+                | KeyCode::PageDown
+                | KeyCode::PageUp
+                | KeyCode::End
+                | KeyCode::Home => {
+                    self.move_point(*code)?;},
+                //Otherwise do nothing
+                _ => (),
+            }
+        }
+        Ok(())
+    }
+
+    fn refresh_screen(&self) -> Result<(), Error> {
+        Terminal::hide_caret()?;
+        Terminal::move_caret(Position::default())?;
+        if self.should_quit {
+            Terminal::clear_screen()?;
+            print!("Goodbye.\r");
+        } else {
+            Self::draw_rows()?;
+            Terminal::move_caret(Position{col: self.location.x, row: self.location.y})?;
+        }
+        Terminal::show_caret()?;
+        Terminal::execute()?;
+        Ok(())
+    }
+
+    fn draw_rows() -> Result<(), Error> {
+        let Size{height, ..} = Terminal::size()?;
+        for row in 0..height {
+            Terminal::clear_line()?;
+            if row == height / 3 {
+                Self::draw_welcome_message()?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+            if row < height.saturating_sub(1) {
+                Terminal::print("\r\n")?;
+            }
+        }
+        Ok(())
+    }
+
+    fn draw_empty_row() -> Result<(), Error> {
+        Terminal::print("~")?;
+        Ok(())
+    }
+
+    fn draw_welcome_message() -> Result<(), Error> {
+        let mut welcome_message = format!("{} editor, v{}", NAME, VERSION);
+        let width = Terminal::size()?.width;
+        let len = welcome_message.len();
+        let padding = (width.saturating_sub(len))/2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        Terminal::print(&welcome_message)?;
+        Ok(())
+    }
+
+    
 
 }
---
+++ log_book/src/editor/terminal.rs
@@ -0,0 +1,83 @@
+use crossterm::cursor::{MoveTo, Hide, Show};
+use crossterm::{queue, Command};
+use crossterm::style::Print;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
+use std::io::{stdout, Error, Write};
+use core::fmt::Display;
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Position {
+    pub col: usize,
+    pub row: usize,
+}
+#[derive(Debug, Clone, Copy)]
+pub struct Size {
+    pub height: usize,
+    pub width: usize,
+}
+
+
+
+pub struct Terminal;
+
+impl Terminal {
+    pub fn initialize() -> Result<(), Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()?;
+        Self::execute()?;
+        Ok(())
+    }
+
+    pub fn terminate() -> Result<(), Error> {
+        Self::execute()?;
+        disable_raw_mode()?;
+        Ok(())
+    }
+
+    pub fn clear_screen() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::All))?;
+        Ok(())
+    }
+
+    pub fn clear_line() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
+        Ok(())
+    }
+
+    pub fn move_caret(position: Position) -> Result<(), Error> {
+        Self::queue_command(MoveTo(position.col as u16, position.row as u16))?;
+        Ok(())
+    }
+
+    pub fn size() -> Result<Size, Error> {
+        let (width_16, height_16) = size()?;
+        let width = width_16 as usize;
+        let height = height_16 as usize;
+        Ok(Size{height, width})
+    }
+
+    pub fn hide_caret() -> Result<(), Error> {
+        Self::queue_command(Hide)?;
+        Ok(())
+    }
+
+    pub fn show_caret() -> Result<(), Error> {
+        Self::queue_command(Show)?;
+        Ok(())
+    }
+
+    pub fn print(message: impl Display) -> Result<(), Error> {
+        Self::queue_command(Print(message))?;
+        Ok(())
+    }
+
+    pub fn execute() -> Result<(), Error> {
+        stdout().flush()?;
+        Ok(())
+    }
+
+    fn queue_command(command: impl Command) -> Result<(), Error> {
+        queue!(stdout(), command)?;
+        Ok(())
+    }
+}
